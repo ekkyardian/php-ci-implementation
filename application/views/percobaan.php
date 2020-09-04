588ea5417da70eca7ad74f66dc9a55273cfdb59b
ekkyardian <ekkyardian@outlook.com>
2020-09-04 14:51:33 +0700

    Upload master project

---
+++ application/views/percobaan.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Percobaan</title>
+</head>
+<body>
+<h1>Ini adalah halaman percobaan</h1>
+<p>
+    Halo, ini adalah halam percobaan. Halaman ini merupakan view pertama saya
+    yang saya buat menggunakan framework Codeigniter. Semoga proses belajar
+    saya menyenangkan.
+</p>
+</body>
+</html>
